938199b8646b668fb74aca16d2c3030476706fc2
Oleksii Moiseenko <>
2020-02-27 20:48:11 +0200

    removed RCKit from deps;

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     ],
     dependencies: [
          .package(url: "https://github.com/techpro-studio/CDPersistence", from: "0.0.2"),
-         .package(url: "https://github.com/techpro-studio/RCKit", from: "0.0.6"),
+         .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RCCD",
-            dependencies: ["RCKit", "CDPersistence"]),
+            dependencies: ["RxSwift", "CDPersistence"]),
     ]
 )
